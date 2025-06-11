e96e3a104a125a8610aca5373f27cdd428a8fc4e
santhosh sivaa v <santhoshsivaav@gmail.com>
2025-06-11 17:50:57 +0530

    final build

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,21 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = (() => {
-    const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-    const { transformer, resolver } = config;
+// Customize the config
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg', 'db', 'mjs', 'cjs'];
 
-    return {
-        ...config,
-        transformer: {
-            ...transformer,
-            babelTransformerPath: require.resolve('react-native-svg-transformer'),
-        },
-        resolver: {
-            ...resolver,
-            assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-            sourceExts: [...resolver.sourceExts, 'svg', 'db', 'mjs', 'cjs'],
-        },
-    };
-})(); 
+module.exports = config; 
